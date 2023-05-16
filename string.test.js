7472f19e9f2703cfb7a32debca866a05290b7d25
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-16 22:54:16 +0300

    Add test for the if statement

--- string.test.js
+++ string.test.js
@@ -2,6 +2,12 @@ const string = require("./string")
 
 
 test("accepts a string and returns its length", ()=>{
-        expect(string("Hi")).toBe(2)
+        expect(string("Hi")).toBe(2);
+    
     }
+)
+test("Throw an Error", ()=>{
+   
+    expect(string("")).toThrow("Invalid string")
+}
 )
